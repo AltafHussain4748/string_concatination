54d48007458634d404ab9dac797680c945b79307
bob <altafhussain4748@gmail.com>
2021-07-20 19:55:27 +0500

    Python package to concate string.

---
+++ README.md
@@ -0,0 +1,18 @@
+A python package to join multiple strings.
+
+Instructions
+
+# To install from python repo
+
+
+1- pip install mdodata
+
+2-from mdodata.mdodata import concat
+
+3- concat('Hello', 'world', sep='.')
+
+
+To install from s3
+
+
+pip install mdodata --extra-index-url='http://mdodata.s3-website.us-east-2.amazonaws.com' --trusted-host='mdodata.s3-website.us-east-2.amazonaws.com'
---
+++ setup.py
@@ -0,0 +1,33 @@
+"""Setup for the mdodata package."""
+
+import setuptools
+
+
+with open('README.md') as f:
+    README = f.read()
+
+setuptools.setup(
+    author="mdodata",
+    author_email="mdodata@gmail.com",
+    name='mdodata',
+    license="MIT",
+    description='chocobo is a python package for data analysis.',
+    version='v0.0.2',
+    long_description=README,
+    url='https://github.com/shaypal5/chocobo',
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+    install_requires=['requests'],
+    classifiers=[
+        # Trove classifiers
+        # (https://pypi.python.org/pypi?%3Aaction=list_classifiers)
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Intended Audience :: Developers',
+    ],
+)
